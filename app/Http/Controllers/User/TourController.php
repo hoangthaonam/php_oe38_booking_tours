4b3aea181b3ef20758fef4a1bb9001ad0469e275
hoangthaonam <hoangthaonampk2407@gmail.com>
2020-12-17 11:09:42 +0700

    TourRepository

--- app/Http/Controllers/User/TourController.php
+++ app/Http/Controllers/User/TourController.php
@@ -7,42 +7,32 @@ use App\Http\Controllers\Controller;
 use App\Models\Tour;
 use App\Models\CommentReview;
 use App\Component\CommentRecursive;
+use App\Repositories\User\Tour\TourRepositoryInterface;
 use Auth;
 
 class TourController extends Controller
 {
+    protected $tourRepo;
+
+    public function __construct(TourRepositoryInterface $tourRepo) {
+        $this->tourRepo = $tourRepo;
+    }
+    
     public function index()
     {
-        $tours = Tour::paginate(config('app.paginate_number'));
+        $tours = $this->tourRepo->getTourPaginate();
         return view('client.pages.tour.index', compact('tours'));
     }
 
     public function show($id)
     {
-        $tour = $this->checkTourExist($id);
+        $tour = $this->tourRepo->checkTourExist($id);
         if($tour){
-            $reviews = CommentReview::with('tour')
-                    ->where('tour_id', $id)
-                    ->where('type', config('app.review_type'))
-                    ->get();
-            $comments = CommentReview::with('user')
-                    ->where('type', config('app.comment_type'))
-                    ->where('tour_id', $id)
-                    ->get();
+            $reviews = $this->tourRepo->getTourReview($id);
+            $comments = $this->tourRepo->getTourComment($id);
             $commentRecursive = new CommentRecursive();
             $comment_data = $commentRecursive->recursive($comments);
             return view('client.layouts.tour_details', compact('tour', 'reviews', 'comment_data'));
-        }
-    }
-
-    public function checkTourExist($id)
-    {
-        $tour = Tour::find($id);
-        if(!$tour){
-            Session::flash('Error', trans('language.error.error_find'));
-            return false;
-        } else {
-            return $tour;
         }
     }
 }
